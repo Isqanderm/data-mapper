87822718a3a6f4362126864ef530572a0ceed335
IsqanderM <aleksandr.melnik.personal@gmail.com>
2024-05-24 13:26:59 +0300

    update package.json. new build

--- build/Mapper.d.ts
+++ build/Mapper.d.ts
@@ -1,10 +1,11 @@
 import { DefaultValues, MappingConfiguration } from "./interface";
-export declare class Mapper<TSource, TTarget> {
+export declare class Mapper<Source, Target> {
     private readonly transformFunction;
     private readonly defaultValues?;
-    constructor(mappingConfig: MappingConfiguration<TSource, TTarget>, defaultValues?: DefaultValues<TTarget>);
+    constructor(mappingConfig: MappingConfiguration<Source, Target>, defaultValues?: DefaultValues<Target>);
     private getValueByPath;
+    private createCompiler;
     private compile;
-    execute(source: TSource): TTarget;
+    execute(source: Source): Target;
 }
 //# sourceMappingURL=Mapper.d.ts.map
--- build/interface.d.ts
+++ build/interface.d.ts
@@ -2,13 +2,17 @@ import type { Mapper } from "./index";
 export type ExcludeMapperProperties<T> = {
     [Key in keyof T as T[Key] extends Mapper<any, any> ? never : Key]: T[Key];
 };
+export type IsObjectWithProperties<T> = T extends Record<string, any> ? true : false;
+export type IsArray<T> = T extends Array<any> ? true : false;
+export type IsString<T> = T extends String ? true : false;
 export type Nullable<T> = {
     [Key in keyof T]: T[Key] | null;
 };
+export type ExtractArrayType<T> = T extends Array<infer U> ? U : T;
 export type DefaultValues<T> = Partial<Nullable<ExcludeMapperProperties<T>>>;
-export type Transformer<T, TResult> = (source: T) => TResult;
-export type DeepPath<Source, K extends keyof Source> = K extends string ? Source[K] extends Record<string, any> ? Source[K] extends Array<any> ? K | `${K}.${DeepPath<Source[K], Exclude<keyof Source[K], keyof any[]>>}` : K | `${K}.${DeepPath<Source[K], keyof Source[K]>}` : K : never;
-export type MappingConfiguration<TSource, TTarget> = {
-    [P in keyof TTarget]: keyof TSource | Transformer<TSource, TTarget[P]> | Mapper<any, any> | DeepPath<TSource, keyof TSource>;
+export type Transformer<T, Result> = (source: T) => Result;
+export type DeepPath<Source, K extends keyof Source> = K extends string ? IsObjectWithProperties<Source[K]> extends true ? IsArray<Source[K]> extends true ? K | `${K}.${number}` | `${K}.${number}.${DeepPath<ExtractArrayType<Source[K]>, keyof ExtractArrayType<Source[K]>>}` : K | `${K}.${DeepPath<Source[K], keyof Source[K]>}` : K : never;
+export type MappingConfiguration<Source, Target> = {
+    [P in keyof Target]: keyof Source | Transformer<Source, Target[P]> | Mapper<any, any> | DeepPath<Source, keyof Source> | MappingConfiguration<Source, Target[P]>;
 };
 //# sourceMappingURL=interface.d.ts.map
